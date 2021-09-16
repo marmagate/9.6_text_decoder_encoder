09d6aea3d3cf7ca930cceadd05e83757708df949
Kuznetsov Igor <kkuznetsov.igor@gmail.com>
2021-09-16 19:18:31 +0300

    now we can use this via *.txt files

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,9 @@
-import java.io.*;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        EncryptionMachine secretInformation = new EncryptionMachine(new StringBuilder("TOP SECRET INFORMATION"));
-
-        FileReader keyReader = new FileReader("keyPhrase.txt");
-        FileOutputStream textDecryptor = new FileOutputStream("decryption.txt");
-
-        char[] buffer = new char[255];
-        int length = keyReader.read(buffer);
-        String keyReaderString = new String(buffer);
-        keyReader.close();
-
-        secretInformation.decrypt(keyReaderString);
-        textDecryptor.write(secretInformation.decryptedText.toString().getBytes());
-        textDecryptor.close();
+        EncryptionMachine encryptionMachine = new EncryptionMachine();
+        encryptionMachine.metaMethod();
     }
 }
